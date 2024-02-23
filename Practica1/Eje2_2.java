4ba60573c2949ea17942fd7ffd808b9245f21e0b
AndrenorAndiarson <letmedieforever@gmail.com>
2024-02-23 08:32:38 -0400

    ya ta

--- Practica1/Eje2_2.java
+++ Practica1/Eje2_2.java
@@ -1,7 +1,22 @@
 package Practica1;
 
+import java.util.Scanner;
+
 public class Eje2_2 {
     public static void main(String[] args) {
         System.out.println("PROMEDIO");
+float C1,C2,C3,C4,S,P;
+        System.out.println("Ingrese la primera nota:");
+        Scanner sc= new Scanner(System.in);
+        C1= sc.nextInt();
+        System.out.println("Ingrese la segunda nota:");
+C2= sc.nextInt();
+        System.out.println("Ingrese la tercera nota:");
+        C3= sc.nextInt();
+        System.out.println("Ingrese la cuarta nota");
+        C4= sc.nextInt();
+        S=C1+C2+C3+C4;
+        P=S/4;
+        System.out.println("El promedio total es: "+P);
     }
 }
