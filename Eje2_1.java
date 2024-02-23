bcfba9aed98ecea07cfebe5207742a2481576891
AndrenorAndiarson <letmedieforever@gmail.com>
2024-02-23 08:17:10 -0400

    Primero

---
+++ Eje2_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Eje2_1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int s;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese un numero:");
+        a = sc.nextInt();
+        System.out.println("Ingrese otro numero:");
+        b = sc.nextInt();
+
+        s = a + b;
+        System.out.println("La suma de los numeros es: "+s);
+    }
+}
