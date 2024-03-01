86b0bc90c9ca9333594c60d3bed1b6b979037e6e
AndrenorAndiarson <letmedieforever@gmail.com>
2024-03-01 11:04:11 -0400

    listo42

---
+++ Practica3/Eje4_2.java
@@ -0,0 +1,23 @@
+package Practica3;
+
+import java.util.Scanner;
+
+public class Eje4_2 {
+    public static void main(String[] args) {
+        System.out.println("SUMA CICLO FOR");
+        double sum,cont;
+        sum=0;
+        cont=1;
+
+        int va;
+        Scanner sc=new Scanner(System.in);
+        do {
+            System.out.println("Ingrese una variable");
+            va= sc.nextInt();
+            sum=sum+va;
+            cont=cont+1;
+}
+while (cont<=10);
+        System.out.println("La suma es: "+sum);
+    }
+}
