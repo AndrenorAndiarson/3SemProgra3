c90aeb4dda3e84bfe2e5375650a148b835fc11e7
AndrenorAndiarson <letmedieforever@gmail.com>
2024-03-08 08:59:35 -0400

    dios al fin otra mas

---
+++ Practica3/Eje4_8.java
@@ -0,0 +1,15 @@
+package Practica3;
+
+public class Eje4_8 {
+    public static void main(String[] args) {
+int n=0;
+do {
+    System.out.println(n);
+    n=n+2;
+
+    }
+while (n<=100);
+
+
+    }
+}
