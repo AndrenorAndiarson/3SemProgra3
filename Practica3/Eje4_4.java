1dcd49a04847cedcdff948c324fd5919f70d93aa
AndrenorAndiarson <letmedieforever@gmail.com>
2024-03-08 08:04:32 -0400

    gracias doc

--- Practica3/Eje4_4.java
+++ Practica3/Eje4_4.java
@@ -6,20 +6,19 @@ public class Eje4_4 {
     public static void main(String[] args) {
         int sum=0;
         int n;
-        int numal,cont;
-        cont=0;
+        int numal;
+        int cont=1;
         double prom;
         Scanner sc= new Scanner(System.in);
         System.out.println("Ingrese el numero de alumnos:");
         numal= sc.nextInt();
-        cont=cont+numal;
         while (cont<=numal){
             System.out.println("Ingrese la edad:");
             n = sc.nextInt();
             sum=sum+n;
+            cont=cont+1;
         }
         prom=sum/numal;
         System.out.println("La edad promedio de los alumnos es: "+prom);
-
     }
 }
