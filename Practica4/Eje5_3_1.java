975c7c1d74e41bfde80e5fbad7164674cde2a8ea
AndrenorAndiarson <shaadowsong@hotmail.com>
2024-03-10 17:16:52 -0400

    sufri demasiado con esto >=(

--- Practica4/Eje5_3_1.java
+++ Practica4/Eje5_3_1.java
@@ -1,44 +1,37 @@
 package Practica4;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Eje5_3_1 {
     public static void main(String[] args) {
         System.out.println("BIG OLD");
-        int n,i,x;
+        int n;
+        int dias=7;
         System.out.println("Ingrese la cantidad de choferes");
         Scanner sc=new Scanner(System.in);
         n= sc.nextInt();
-        char nombres [][]=new char[n][30];
-        int kil[][]=new int[n][6];
-        char [][] dias =new char [5][];
-dias[][]="Lunes","Martes","Miercoles","Jueves","Viernes","Sabado" ;
-        };
-for (i = 0; i < n; i++)//filas
-    {
-        printf("\nIntroduzca el nombre del chofer: %d\n", i + 1);
-        scanf("%s", nombres[i]);
-
-        for (x = 0; x < 6; x++)//columnas
-        {
-            printf("\nIntroduzca la velocidad de kilometros a los que conduce:\n");
-            scanf("%d", &kilometros[i][x]);
+        int datosChof [][]= new int[n][dias];
+        int totkm[]= new int[n];
+        int km=0;
+for (int i=0;i<n;i++) {
+    System.out.println("Ingrese los km recorridos por el chofer: "+(i + 1)+ " durante la semana");
+        for (int j=0;j<7;j++) {
+            System.out.println("Dia "+(j+1)+" : ");
+            dias= sc.nextInt();
         }
     }
-
-  for (i = 0; i < n; i++)//filas
-    {
-        int suma[i];
-        suma[i] = 0;
-        printf("\nConductor %d\n", i + 1);
-        printf("Nombre: %s\n", nombres[i]);
-        suma[i] += kilometros[i][x];
-        for (x = 0; x < 6; x++)//columnas
-        {
-            printf("Dia: %s\n", dias[x]);
-            printf("%d Km\n", kilometros[i][x]);
-        }
-        printf("El total de kilometros recorridos en la semana: %d\n\n", suma[i]);
+for (int i=0;i<totkm.length;i++){
+    km=km+dias;
+for (int j=0;j<7;j++){
+    totkm [i] =totkm [i]+ km;
     }
+}
+
+        System.out.println("Total de km recirridos por cada chofer");
+for (int i=0;i<n;i++){
+    System.out.println(" Chofer: "+ (i+1)+ " : "+ totkm[i]+" km");
+
+}
+
     }
 }
